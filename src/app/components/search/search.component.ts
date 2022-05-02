3af41513781fd0269996a6e0bf1bb796bfc0957b
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-05-02 16:20:18 -0600

    tried to get the instructions to work

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -12,18 +12,27 @@ import { HttpClient } from '@angular/common/http';
 
 
 export class SearchComponent implements OnInit {
-  text: any = [];
- 
+  name: any = [];
+  instructions: any = [];
+  id!: string;
+  first!: string;
+  second!: string;
+  third!: string;
+
+
 
   constructor(
     private http: HttpClient,
-) { }
+) { } 
 
 
   ngOnInit(): void {
 
+  // this.searchByIngredient();
 
+  }
 
+  searchByIngredient(first: string, second: string, third: string) {
     const options = {
       method: 'GET',
       headers: {
@@ -33,15 +42,42 @@ export class SearchComponent implements OnInit {
       }
     };
   
-      this.http.get('https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apples%2Cflour%2Csugar&number=10&ignorePantry=true&ranking=1', options)
+      this.http.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=${first}%2C${second}%2C${third}&number=10&ignorePantry=true&ranking=1`, options)
       .subscribe(data => {
-        this.text = data
+        this.name = data
       })
-
   }
 
   onSubmit() {
     console.log("submited")
+    // let first = document.querySelector("#first");
+    // console.log(this.first);
+    // console.log(this.second);
+    // console.log(this.third);
+    this.searchByIngredient(this.first, this.second, this.third);
+
+  }
+
+  checkIcon(index: string) {
+    console.log("id",index)
+    this.instructionsById(index);
+  }
+
+
+  instructionsById(id: string) {
+    const options = {
+      method: 'GET',
+      headers: {
+        
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+        'X-RapidAPI-Key': `${environment.recipe}`
+      }
+    };
+  
+      this.http.get(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`, options)
+      .subscribe(data => {
+        this.instructions = data
+      })
   }
  
 }
