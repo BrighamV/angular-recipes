ec63d722e2c962b73b740a680adc32f2493dfb87
Brigham Valentine <71526640+BrighamV@users.noreply.github.com>
2022-06-10 12:46:49 -0600

    heroku is difficult

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.use(express.static(__dirname + '/dist/angular-recipe'))
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+
         '/dist/angular-recipe/index.html')); });
-app.listen(precess.env.PORT || 8080)
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 8000;
+}
+app.listen(port);
